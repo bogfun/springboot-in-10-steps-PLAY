1e80d9b79788d2cb3f22fe6fd636a9feae18b318
Bogdan.Chicos <Bogdan.Chicos@vauban.ro>
2018-02-27 18:49:35 +0200

    Small bug fix. Arrays was from the correct package.

--- src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
+++ src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -1,9 +1,8 @@
 package com.in28minutes.springboot.basics.springbootin10steps;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,11 +11,7 @@ public class BooksController {
 
 	@GetMapping("/books")
 	public List<Book> getAllBooks() {
-		List<Object> asList = Arrays.asList(new Book(1l, "This is it !!!", "Michael Jackson"));
-		List<Book> books = new ArrayList<>();
-		for(Object elem : asList) {
-			books.add((Book) elem);
-		}
+		List<Book> books = Arrays.asList(new Book(1l, "This is it !!!", "Michael Jackson"), new Book(2l, "Zoukdan !!!", "Bogdan Chicos"));
 		return books;
 	}
 
